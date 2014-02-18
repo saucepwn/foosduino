51f7eb0a8c67897791231b1b729901f0af02f567
pwnsauce <RPG405@gmail.com>
2014-02-17 22:33:43 -0800

    Timer works. Look at optimizing the calls to change the minute display, right now it's called every second when realistically it only needs to be called every minute.
    Signed-off-by: pwnsauce <RPG405@gmail.com>

--- Foosball_Scoreboard_Arduino_Component/Foosball_Scoreboard_Arduino_Component.ino
+++ Foosball_Scoreboard_Arduino_Component/Foosball_Scoreboard_Arduino_Component.ino
@@ -1,20 +1,32 @@
 #include <genieArduino.h>
 
 /* Define pins */
+const int clockInterrupt = 0;  // For Arduino Uno, interrupt 0 is pin 2.
+const int timerOutputPin = 3;  // A PWM pin which will generate a square wave for our timer.
+const int displayResetPin = 4;
 const int blackScorePin = 8;
 const int yellowScorePin = 9;
 const int ballInsertPin = 10;
 const int ledPin =  13;
 
-// Game status variables.
+/* Game status variables. */
 int blackScore = 0;
 int yellowScore = 0;
 int playerARounds = 0;
 int playerBRounds = 0;
 int ballInPlay = 0;
 
-// Game config variables.
+/* Clock variables / config */
+const long UPDATE_CLOCK_CYCLES = 10000;      // Check for the update clock flag every 10,000 cycles.
+long loopCycleCount = 0;
+volatile boolean updateClockFlag = false;
+volatile int clockSeconds;
+volatile int clockMinutes;
+int pulseCount;
+
+/* Game config variables. */
 const int POINTS_PER_MATCH = 5;
+const int AFTER_ROUND_PAUSE_MSEC = 9000;    // The amount of time to freeze the scoreboard after each round, in milliseconds.
 
 void setup()
 {
@@ -22,43 +34,63 @@ void setup()
   pinMode(blackScorePin, INPUT_PULLUP);
   pinMode(yellowScorePin, INPUT_PULLUP);
   pinMode(ballInsertPin, INPUT_PULLUP);
- 
+   
   // Set up the LED pin to be an output:
   pinMode(ledPin, OUTPUT);
   
-  // Set up the display.
   initDisplay();
 }
  
 void loop()
-{ 
-  if (digitalRead(ballInsertPin) == LOW && ballInPlay == 0)
+{
+  // Either poll for a score or a ball in play, depending on whether or not a ball is in play.
+  if (ballInPlay == 0) pollForBallInsert();
+  else pollForScore();
+  
+  if (loopCycleCount == UPDATE_CLOCK_CYCLES)
+  {
+    if (updateClockFlag) updateClockDisplay();
+    loopCycleCount = 0;
+  }
+  loopCycleCount++;
+}
+
+/**
+ * Poll the ball insert pin which would mean a ball was inserted into play. Assumes
+ * a ball is not currently in play.
+ */
+void pollForBallInsert()
+{
+  if (digitalRead(ballInsertPin) == LOW)
   {
     ballInPlay = 1;
     genieWriteStr(0, "Ball is in play");
-    digitalWrite(ledPin, HIGH);
+    startClock(true);
   }
-  
-  if (ballInPlay == 1) pollForScore();
 }
 
+/**
+ * Poll the scoreboard pins for input which would mean a player has scored.
+ */
 void pollForScore()
 {
   if (digitalRead(blackScorePin) == LOW)
   {
+    stopClock(false);
+    ballInPlay = 0;
+    
     blackScore++;
     genieWriteObject(GENIE_OBJ_LED_DIGITS, 1, blackScore);
     if (blackScore != POINTS_PER_MATCH) genieWriteStr(0, "Black scores!");
-
-    ballInPlay = 0;
   }
   else if (digitalRead(yellowScorePin) == LOW)
   {
+    stopClock(false);
+    ballInPlay = 0;
+    
     yellowScore++;
     genieWriteObject(GENIE_OBJ_LED_DIGITS, 0, yellowScore);
     if (yellowScore != POINTS_PER_MATCH) genieWriteStr(0, "Yellow scores!");
-    
-    ballInPlay = 0;
   }
   
   // Check to see if the match is over.
@@ -107,7 +139,7 @@ void updateMatchScores(int winningTeam)
   }
   
   // Give teams a chance to switch sides.
-  delay(9000);
+  delay(AFTER_ROUND_PAUSE_MSEC);
   
   // Show a swapped match score for when the players switch sides.
   if (oddRound)
@@ -140,16 +172,86 @@ void updateMatchScores(int winningTeam)
   }
 }
 
+/**
+ * Starts a 50% duty cycle on a PWM pin.
+ */
+void startClock(boolean reset)
+{
+  if (reset)
+  {
+    clockSeconds = 0;
+    clockMinutes = 0;
+    updateClockDisplay();
+  }
+
+  pulseCount = 0;  
+  attachInterrupt(clockInterrupt, clockTick, RISING);
+  analogWrite(timerOutputPin, 127);
+}
+
+/**
+ * Called as an Interrupt Handler Routine on each clock pulse. Update the clock
+ * on the display if one second has elapsed.
+ */
+void clockTick()
+{
+  pulseCount++;
+  if (pulseCount == 489)  // 490Hz reached
+  {
+    clockSeconds++;
+    if (clockSeconds == 60)
+    {
+      clockMinutes++;
+      clockSeconds = 0;
+    }
+    
+    pulseCount = 0;
+    updateClockFlag = true;
+  }
+}
+
+/**
+ * Updates the clock on the display.
+ */
+void updateClockDisplay()
+{
+  genieWriteObject(GENIE_OBJ_LED_DIGITS, 4, clockMinutes);
+  genieWriteObject(GENIE_OBJ_LED_DIGITS, 5, clockSeconds);
+  updateClockFlag = false;
+}
+
+/**
+ * Stop the clock from ticking.
+ * @param reset True to reset the digits on the clock.
+ */
+void stopClock(boolean reset)
+{
+  detachInterrupt(clockInterrupt);
+  analogWrite(timerOutputPin, 0);
+  
+  if (reset)
+  {
+    clockSeconds = 0;
+    clockMinutes = 0;
+    updateClockDisplay();
+  }
+  
+  pulseCount = 0;
+}
+
+/**
+ * Initializes the Genie LCD display.
+ */
 void initDisplay()
 {
   genieBegin (GENIE_SERIAL, 115200);
   //genieAttachEventHandler(myEventHandler);
   
   // ResetSend a reset signal to the display then wait for it to come up.
-  pinMode(4, OUTPUT);
-  digitalWrite(4, 1);
+  pinMode(displayResetPin, OUTPUT);
+  digitalWrite(displayResetPin, 1);
   delay(100);
-  digitalWrite(4, 0);
+  digitalWrite(displayResetPin, 0);
   delay(3000);
   
   genieWriteStr(0, "Ready");
